adb309f89240bd9bea36293ed076b129e8f9150c
Janice Xu <janicexu@garibaldi00.scripps.edu>
2016-12-07 16:08:53 -0800

    create download.sh

---
+++ scripts/download.sh
@@ -0,0 +1,3 @@
+#UniProt - accessed 11/30/2016; last updated 11/30/2016 
+wget "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/uniprot_sprot_*.dat.gz" -nv -a download.log 
+wget "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/uniprot_trembl*.dat.gz" -nv -a download.log
