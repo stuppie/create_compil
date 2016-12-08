bba325a204f2705ca1968d3d111daa9130033989
Janice Xu <janicexu@garibaldi00.scripps.edu>
2016-12-07 17:29:41 -0800

    edit download.sh  enter the commit message for your changes. Lines starting

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,3 +1,22 @@
+set -e
 #UniProt - accessed 11/30/2016; last updated 11/30/2016 
-wget "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/uniprot_sprot_*.dat.gz" -nv -a download.log 
-wget "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/uniprot_trembl*.dat.gz" -nv -a download.log
+wget "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/uniprot_sprot_*.dat.gz" -P ~/create_compil/scripts/data -nv -a download.log 
+wget "ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/uniprot_trembl*.dat.gz" -P ~/create_compil/scripts/data -nv -a download.log
+
+#MetaHit-Qin, et al 2010 - accessed 12/5/2016; last updated 06/2009
+wget "http://www.bork.embl.de/~arumugam/Qin_et_al_2010/BGI_GeneSet20090523.fa.gz" -P ~/create_compil/scripts/data -nv -a download.log 
+wget "http://www.bork.embl.de/~arumugam/Qin_et_al_2010/BGI_GeneSet20090523_annotation.fa.gz" -P ~/create_compil/scripts/data -nv -a download.log 
+wget "http://www.bork.embl.de/~arumugam/Qin_et_al_2010/BGI_GeneSet20090523_taxonomic.fa.gz" -P ~/create_compil/scripts/data -nv -a download.log 
+wget "http://www.bork.embl.de/~arumugam/Qin_et_al_2010/frequent_microbe_proteins.fasta.gz" -P ~/create_compil/scripts/data -nv -a download.log 
+
+#RefSeq - accessed 12/5/2016; last updated 11/3/2016
+wget "ftp://ftp.ncbi.nlm.nih.gov/refseq/release/bacteria/bacteria.nonredundant*.protein.gpff.gz" -P ~/create_compil/scripts/data -nv -a download.log 
+wget "ftp://ftp.ncbi.nlm.nih.gov/refseq/release/archaea/archaea.nonredundant*.protein.gpff.gz" -P ~/create_compil/scripts/data -nv -a download.log 
+wget "ftp://ftp.ncbi.nlm.nih.gov/refseq/release/fungi/fungi.*.protein.gpff.gz" -P ~/create_compil/scripts/data -nv -a download.log 
+wget "ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.*.protein.gpff.gz" -P ~/create_compil/scripts/data -nv -a download.log 
+
+#HMPI Stool - accessed 12/5/2016; last updated 2011
+wget "ftp://public-ftp.hmpdacc.org/HMGI/stool/*.with_fasta.gff3.bz2" -P ~/create_compil/scripts/data -nv -a downoad.log 
+
+#cRAPome - accessed 12/6/2016; last updated 01/30/2015
+wget "ftp://ftp.thegpm.org/fasta/crap/crap.fasta" -P ~/create_compil/scripts/data -nv -a download.log 
